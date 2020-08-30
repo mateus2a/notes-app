a46cfd01a4148b176e227b80290589cb03ff7aa1
Mateus Alencar <mtauws@gmail.com>
2020-08-30 17:13:31 -0300

    NoteController is maded

--- backend/src/controllers/NoteController.js
+++ backend/src/controllers/NoteController.js
@@ -1,11 +1,31 @@
-const Note = require('../models/Note')
+const Note = require('../models/Note');
 
 module.exports = {
+  async index(req, res) {
+    const notes = await Note.findAll();
+
+    return res.json(notes)
+  },
+
   async store(req, res) {
-    const { text } = req.body;
+    const note = await Note.create(req.body)
 
-    const note = await Note.create({text})
+    return res.send(note)
+  },
 
-    return res.json(note)
+  async update(req, res) {
+    const note = await Note.findByPk(req.params.id)
+    
+    await note.update(req.body)
+
+    return res.json({ note })
+  },
+
+  async destroy(req, res) {
+    const note = await Note.findByPk(req.params.id)
+
+    await note.destroy()
+
+    return res.json()
   }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,10 @@ const NoteController = require('./controllers/NoteController')
 
 const routes = express.Router()
 
+routes.get('/notes', NoteController.index)
 routes.post('/notes', NoteController.store)
+routes.put('/notes/:id', NoteController.update)
+routes.delete('/notes/:id', NoteController.destroy)
 
 
 module.exports = routes
