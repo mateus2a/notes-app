7a728980beb3ec379b88548c0057f9c143bac87a
Mateus Alencar <mtauws@gmail.com>
2020-08-31 17:14:31 -0300

    Initied notes page

---
+++ frontend/src/pages/Notes/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import './styles.css';
+
+function Notes() {
+  return (
+    <>
+      <section>
+        <div className="notes">
+          <div className="note-wrapper">
+            <div className="title">
+              <p>Lembretes</p>
+            </div>
+            <ul className="text">
+              <li>Lembrar de ser feliz</li>
+            </ul>
+          </div>
+          <div className="note-wrapper">
+            <div className="title">
+              <p>Lembretes</p>
+            </div>
+            <ul className="text">
+              <li>Lembrar de ser feliz</li>
+            </ul>
+          </div>
+          <div className="note-wrapper">
+            <div className="title">
+              <p>Lembretes</p>
+            </div>
+            <ul className="text">
+              <li>Lembrar de ser feliz</li>
+            </ul>
+          </div>
+          <div className="note-wrapper">
+            <div className="title">
+              <p>Lembretes</p>
+            </div>
+            <ul className="text">
+              <li>Lembrar de ser feliz</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Notes;
